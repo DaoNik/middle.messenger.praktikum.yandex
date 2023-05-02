d3af4c71889c6f348badfa1051e70c3ee41e37e2
DaoNik <nikitataranin@gmail.com>
2023-05-02 23:43:30 +0300

    feat: add basic config

---
+++ src/template.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const stringTmpl = fs.readFileSync(`${__dirname}/index.html`, 'utf8');
+
+// const regExp = new RegExp(/{{.*}}/gm);
+// const keys = stringTmpl.match(regExp);
+// const values = stringTmpl.match(/--- templater ---.*/gms)
+// console.log(values);
+// const map = new Map();
+// keys
+//   .map(key => key.slice(2, key.length - 2))
+//   .forEach((key) => {
+//     const keyValue = values[0].match(key);
+//     console.log(keyValue);
+//   });
+
+const keys = stringTmpl.match(/{{>.*}}/gm);
+keys
+  .map(key => key.slice(3, key.length - 2))
+  .forEach((key) => {
+    const file = fs.readFileSync(`${__dirname}${key}`, 'utf8');
+    const result = stringTmpl.replace(/{{>.*}}/, file);
+    fs.writeFile(`${__dirname}/result.html`, result, 'utf8', () => {});
+  });
