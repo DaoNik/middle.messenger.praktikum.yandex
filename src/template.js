47d02383f61308406892e0610758ba71385252a4
DaoNik <nikitataranin@gmail.com>
2023-05-03 00:09:52 +0300

    feat: add page 404 and improve templater

--- src/template.js
+++ src/template.js
@@ -15,10 +15,13 @@ const stringTmpl = fs.readFileSync(`${__dirname}/index.html`, 'utf8');
 //   });
 
 const keys = stringTmpl.match(/{{>.*}}/gm);
+let result = stringTmpl;
 keys
   .map(key => key.slice(3, key.length - 2))
   .forEach((key) => {
     const file = fs.readFileSync(`${__dirname}${key}`, 'utf8');
-    const result = stringTmpl.replace(/{{>.*}}/, file);
-    fs.writeFile(`${__dirname}/result.html`, result, 'utf8', () => {});
+    const regExp = new RegExp(`{{>${key}}}`, 'gm')
+    result = result.replace(regExp, file);
   });
+
+fs.writeFile(`${__dirname}/result.html`, result, 'utf8', () => {});
